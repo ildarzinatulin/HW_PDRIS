977b706fac7723e0df543f756e4aa9c87b3821ca
Ildar Zinatulin <zinatulin@yandex-team.ru>
2020-12-14 00:35:56 +0300

    add test

---
+++ src/test/java/com/example/application/service/PredictCurrencyServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.application.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PredictCurrencyServiceTest {
+
+    @Test
+    public void predict() {
+        int a = 5;
+        a += 1;
+    }
+}
